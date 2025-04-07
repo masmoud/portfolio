6ac369bac566575eb152d93d52a9cf7b09ea3b96
Mohamed Amoussa <mohamedj81@hotmail.com>
2025-04-07 23:34:00 +0200

    Implement Home section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { LoadingScreen } from "./components/LoadingScreen";
 import { MobileMenu } from "./components/MobileMenu";
 import { Navbar } from "./components/Navbar";
 import "./index.css";
+import { Home } from "./components/sections/Home";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -34,6 +35,7 @@ function App() {
         } bg-black text-gray-100 `}>
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Home />
       </div>
     </>
   );
