25618f97779d050bf5bfee6d918810a4cd3f0577
Mohamed Amoussa <mohamedj81@hotmail.com>
2025-04-07 23:18:45 +0200

    Implement responsive navbar

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { LoadingScreen } from "./components/LoadingScreen";
+import { MobileMenu } from "./components/MobileMenu";
+import { Navbar } from "./components/Navbar";
+import "./index.css";
+
+function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 768) {
+        setMenuOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
+  return (
+    <>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+
+      <div
+        className={`min-h-screen transition-opacity duration-700 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        } bg-black text-gray-100 `}>
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      </div>
+    </>
+  );
+}
+
+export default App;
